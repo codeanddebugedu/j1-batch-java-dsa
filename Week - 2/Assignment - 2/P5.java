79f625ffe00cd2f7b699bf6246d2839694953354
nilay thakkar <nilay2620@gnail.com>
2024-05-17 17:21:03 +0530

    Week - 2 Assignment - 3 solution uploaded

--- Week - 2/Assignment - 2/P5.java
+++ Week - 2/Assignment - 2/P5.java
@@ -7,11 +7,11 @@ public class P5
         System.out.print("Enter Number: ");
         int x=sc.nextInt();
         int cnt=0;
-        while(x!=0)
+        do
         {
             cnt=cnt+1;
             x=x/10;
-        }
+        }while(x!=0);
         System.out.println("No of Digits are: "+cnt);
         sc.close();
     }    
